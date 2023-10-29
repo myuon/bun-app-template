44db6a080e8e77c8c15182008ed953a30d4997fe
myuon <ioi.joi.koi.loi@gmail.com>
2023-10-29 22:33:18 +0900

    feat: serve index.html

--- index.ts
+++ index.ts
@@ -1,7 +1,15 @@
 const server = Bun.serve({
   port: 3000,
-  fetch(req) {
-    return new Response("Bun!!");
+  async fetch(req) {
+    const path = new URL(req.url).pathname;
+    const file = Bun.file(
+      // spa fallback
+      path === "/" ? "index.html" : path
+    );
+    if (!(await file.exists())) {
+      return new Response("Not found", { status: 404 });
+    }
+    return new Response(file);
   },
 });
 
