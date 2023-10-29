85438a2054ccc5fb7d67c5ef4d28bb28803c859d
myuon <ioi.joi.koi.loi@gmail.com>
2023-10-30 00:15:05 +0900

    chore: watch and restart bun server

--- dev.ts
+++ dev.ts
@@ -1,16 +1,52 @@
 import process from "process";
+import { watch } from "fs";
+
 process.env.NODE_ENV = "development";
 
-const viteServer = Bun.spawn(["bun", "dev"], {
-  cwd: "./web",
+const startViteServer = () => {
+  const viteServer = Bun.spawn(["bun", "dev"], {
+    cwd: "./web",
+  });
+
+  console.log(`vite dev server has started`);
+
+  return viteServer;
+};
+
+const startBunServer = () => {
+  const bunServer = Bun.spawn(["bun", "index.ts"], {
+    stdout: "inherit",
+  });
+
+  console.log(`bun server has been started`);
+
+  return bunServer;
+};
+
+const viteServer = startViteServer();
+let bunServer = startBunServer();
+
+const watcher = watch(
+  import.meta.dir,
+  { recursive: true },
+  (event, filename) => {
+    if (typeof filename === "string") {
+      // restart backend server
+      if (filename === "index.ts" || filename.startsWith("api/")) {
+        console.log(`file ${filename} changed`);
+
+        bunServer.kill();
+        bunServer = startBunServer();
+      }
+    }
+  }
+);
+
+process.on("SIGINT", () => {
+  console.log("SIGINT received");
+  watcher.close();
+  viteServer.kill();
+  bunServer.kill();
+
+  process.exit(0);
 });
-
-console.log(`vite dev server has been started`);
-
-const bunServer = Bun.spawn(["bun", "index.ts"], {
-  stdout: "inherit",
-});
-
-console.log(`bun server has been started`);
-
-console.log(await new Response(bunServer.stdout).text());
